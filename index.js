a18eaa4a19ebbc7ed63c75bc4f5d2434481a5537
Ubuntu <ubuntu@ip-172-31-94-114.ec2.internal>
2019-08-27 18:35:54 +0000

    init app

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+* File name: index.js
+* Created by Visual studio code
+* User: Danh Le / danh.danh20051995@gmail.com
+* Date: 2019-01-19 04:04:57
+*/
+'use strict'
+const express = require('express')
+const app = express()
+const os = require('os')
+
+app.get('*', (req, res) => {
+  let response = {}
+  for (let index = 0; index < 5; index++) {
+    response[Math.random()] = 'Random number: ' + Math.random()
+  }
+
+  return res.send(response)
+})
+
+app.listen(3002, '0.0.0.0', () => console.log('App listening at http://%s:%s', os.hostname(), 3002))
+
